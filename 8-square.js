b8b4445cdfaebbdfb2f7d4365c881bc2b94c8a3e
Dase Etadafe <daseetadafe56@gmail.com>
2025-05-25 20:15:27 +0100

    update 8-square.js: use while and for loop with single console.log

--- 8-square.js
+++ 8-square.js
@@ -4,7 +4,13 @@ if(isNaN(size) || size <= 0){
     console.log("Missing size");
 }
 else {
-    for(let i = 0; i < size; i++){
-        console.log("x".repeat(size));
+    let i = 0;
+    while(i < size){
+        let row = "";
+        for(let j = 0; j < size; j++){
+            row += "X";
+        }
+        console.log(row);
+        i++;
     }
 }
