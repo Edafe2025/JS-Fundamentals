d7091e492ba53a1a59df79587d4b3379c3930560
Dase Etadafe <daseetadafe56@gmail.com>
2025-05-25 21:45:07 +0100

    Fix: use while and for loops in 8-square_clean.js

---
+++ 8-square_clean.js
@@ -0,0 +1,16 @@
+const size = parseInt(process.argv[2]);
+
+if(isNaN(size) || size <= 0){
+    console.log("Missing size");
+}
+else {
+    let i = 0;
+    while(i < size){
+        let row = "";
+        for(let j = 0; j < size; j++){
+            row += "X";
+        }
+        console.log(row);
+        i++;
+    }
+}
