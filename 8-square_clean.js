2c1accb3185cd81d4abc0c005f919cdf4bafc82c
Dase Etadafe <daseetadafe56@gmail.com>
2025-05-26 12:41:30 +0100

    Add 8-Square_clean.js with while loop

--- 8-square_clean.js
+++ 8-square_clean.js
@@ -1,16 +1,12 @@
 const size = parseInt(process.argv[2]);
 
-if(isNaN(size) || size <= 0){
+if (isNaN(size)){
     console.log("Missing size");
 }
-else {
-    let i = 0;
+else{
+    let i = 0
     while(i < size){
-        let row = "";
-        for(let j = 0; j < size; j++){
-            row += "X";
-        }
-        console.log(row);
+        console.log("X".repeat(size));
         i++;
     }
 }
