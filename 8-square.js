86511e1f7dd4d5e2334b6a36dee5716a553da141
Dase Etadafe <daseetadafe56@gmail.com>
2025-05-26 15:30:42 +0100

    update 8-square.js using while loop

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,12 @@
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size)){
+if (isNaN(size) || size <= 0) {
     console.log("Missing size");
 }
-else{
-    for(let i = 0; i < size; i++){
+else {
+    let i = 0;
+    while (i < size) {
         console.log("X".repeat(size));
+        i++;
     }
 }
